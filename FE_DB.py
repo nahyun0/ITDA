b95d83538e421e12c08b2556158cf01ddae8f011
kimnahyun <106727030+nahyun0@users.noreply.github.com>
2025-03-15 00:30:37 +0900

    Update FE_DB.py

--- FE_DB.py
+++ FE_DB.py
@@ -1,26 +1,7 @@
 
 # -*- coding: utf-8 -*-
-# 3.14~ 
 
-# cd ITDA\itda_2
-# install : (base) C:\Users\nahyun\ITDA\itda_2>pip install streamlit
-# Run : (base) C:\Users\nahyun\ITDA\itda_2>streamlit run itda_2.py
-
-
-
-# 2) simple sign-up process (Streamlit)
-#import streamlit as st
-
-#st.title("Sign Up")
-
-#id = st.text_input("Enter your ID")
-#password = st.text_input("Enter your password", type="password")
-
-#if st.button("Sign Up"):
-#   st.success(f"Sign up request: {id}")
-
-
-#4) FE <-> DB Connect 
+#4) FE <-> DB 연결 확인 해본 것 ( id, password 입력시 mySQL 쪽에서 저장이 되는 지) 
 
 import streamlit as st
 from DB_BE import register_user 
