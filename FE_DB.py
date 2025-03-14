b16701903e1a87f69bf0959d3a324c04a3c78e1c
kimnahyun <106727030+nahyun0@users.noreply.github.com>
2025-03-15 00:49:08 +0900

    Update FE_DB.py

--- FE_DB.py
+++ FE_DB.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #4) FE <-> DB 연결 확인 해본 것 ( id, password 입력시 mySQL 쪽에서 저장이 되는 지) 
-#  Streamlit을 사용하여 사용자 인터페이스(UI)를 담당하는 부분 
+#  Streamlit을 사용하여 사용자 인터페이스(UI)를 담당하는 부분 관련 
 
 # pip install streamlit (설치)
 # streamlit run FE_DB.py (실행) 
